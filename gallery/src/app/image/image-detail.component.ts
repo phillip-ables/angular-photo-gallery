5cc48b4f332b84ac930eba3d26c3ff7d9eb948c0
philables <phillipables1@gmail.com>
2018-01-02 22:51:13 -0500

    image-detail component on init set route for image to instance of image id

--- gallery/src/app/image/image-detail.component.ts
+++ gallery/src/app/image/image-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ImageService } from './shared/image.service';
 
 @Component ({
@@ -7,7 +7,14 @@ import { ImageService } from './shared/image.service';
 	styleUrls: ['./image-detail.component.css']
 })
 
-export class ImageDetailComponent {
+export class ImageDetailComponent implements OnInit {
 	// create an image service that is going to get injected into that service into the vector of this image class
+	image:any
 	constructor(private imageService: ImageService) { }
+
+	ngOnInit(){
+		this.image = this.imageService.getImage(
+				+this.route.snapshot.params['id']
+			)
+	}
 };
