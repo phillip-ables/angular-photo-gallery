ae990810d8efed4c2ec4fb2ada382e9c04db8062
philables <phillipables1@gmail.com>
2018-01-02 22:46:04 -0500

    import RouteModule and set forRoot to appRoutes and implement router-outlet instead of app-gallery in app component html

--- gallery/src/app/app.module.ts
+++ gallery/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
-import { ImageFilterPipe } from './image/shared/filter.pipe'
+import { ImageFilterPipe } from './image/shared/filter.pipe';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ImageDetailComponent } from './image/image-detail.component';
 import { ImageService } from './image/shared/image.service';
+import { appRoutes } from '../routes';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import { ImageService } from './image/shared/image.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ImageService, ImageFilterPipe],
   bootstrap: [AppComponent]
