e04e1fa8b3b3172ddb39e9ea5b7970291a74d726
philables <phillipables1@gmail.com>
2018-01-02 22:33:40 -0500

    add routes.ts to store the paths to route too

--- gallery/src/app/image/shared/filter.pipe.ts
+++ gallery/src/app/image/shared/filter.pipe.ts
@@ -6,6 +6,6 @@ export class ImageFilterPipe implements PipeTransform{
 		if(criteria === 'all') {return items } else
 			return items.filter(item => {
 				return item.category === criteria;
-			})
+			});
 	}
 }
